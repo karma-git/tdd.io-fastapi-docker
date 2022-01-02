89b232fea0deaeecdc7aa8bfa5e1364e5ae8f250
Andrew Horbach <andrewhorbach@gmail.com>
2022-01-02 06:53:49 +0300

    chore: step POST Route

---
+++ project/app/api/crud.py
@@ -0,0 +1,14 @@
+# project/app/api/crud.py
+
+
+from app.models.pydantic import SummaryPayloadSchema
+from app.models.tortoise import TextSummary
+
+
+async def post(payload: SummaryPayloadSchema) -> int:
+    summary = TextSummary(
+        url=payload.url,
+        summary="dummy summary",
+    )
+    await summary.save()
+    return summary.id
