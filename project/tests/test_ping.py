78858710ee5df51f5407f0b7c72670e13ea1adc0
Andrew Horbach <andrewhorbach@gmail.com>
2022-01-05 02:27:13 +0300

    chore: add cd

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -7,4 +7,4 @@ from app import main  # noqa: F401
 def test_ping(test_app):
     response = test_app.get("/ping")
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
+    assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
