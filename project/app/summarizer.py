3a34186baf625b735c715d8e425a95feb7c495cd
Andrew Horbach <andrewhorbach@gmail.com>
2022-01-06 13:52:44 +0300

    chore: step Background Task

---
+++ project/app/summarizer.py
@@ -0,0 +1,20 @@
+# project/app/summarizer.py
+
+
+import nltk
+from newspaper import Article
+
+
+def generate_summary(url: str) -> str:
+    article = Article(url)
+    article.download()
+    article.parse()
+
+    try:
+        nltk.data.find("tokenizers/punkt")
+    except LookupError:
+        nltk.download("punkt")
+    finally:
+        article.nlp()
+
+    return article.summary
